7fa137324f6044db7c9e152a769f4bac8f874765
Vestein Dahl <vesteindahl@hotmail.com>
2016-11-17 23:48:06 +0100

    Init.

---
+++ desktop/src/no/vestein/sudoku/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.vestein.sudoku.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.vestein.sudoku.Sudoku;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sudoku(), config);
+	}
+}
