22cd264a8bc408a9038dfbe6274b74327b7d0a8f
Vestein Dahl <vesteindahl@hotmail.com>
2016-11-21 00:20:10 +0100

    Sets screen width and height.

--- desktop/src/main/java/no/vestein/sudoku/desktop/DesktopLauncher.java
+++ desktop/src/main/java/no/vestein/sudoku/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package no.vestein.sudoku.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.vestein.sudoku.Reference;
 import no.vestein.sudoku.Sudoku$;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Reference.WIDTH();
+		config.height = Reference.HEIGHT();
+		config.samples = 0;
 		new LwjglApplication(Sudoku$.MODULE$, config);
 	}
 }
